751e9529e284a6aabc8511b207519d1b9985a4ff
gaberabreu <gaberabreu.github@gmail.com>
2024-11-17 17:59:33 -0300

    Initial mui setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
+
 const App = () => {
-  return <h1>App.tsx</h1>;
+  const theme = createTheme();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Typography variant="h1">App.tsx</Typography>
+    </ThemeProvider>
+  );
 };
 
 export default App;
